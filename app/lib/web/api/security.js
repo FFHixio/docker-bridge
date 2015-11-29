9ac788ada13ce1c4d4018c8a7bf2c53c298e23ba
Corey Butler <corey@coreybutler.com>
2015-11-28 23:32:46 -0600

    Refactored routes to API modules

---
+++ app/lib/web/api/security.js
@@ -0,0 +1,38 @@
+'use strict'
+
+let crypto = require('crypto')
+let validTokenDuration = process.env.TOKEN_VALIDITY_DURATION || (3 * 60 * 60 * 1000) // 3 hours by default
+
+module.exports = function (app) {
+  return {
+    token: function (req, res) {
+      let credential = req.credential.split(':') // Get the basic auth apply
+
+      try {
+        if (req.type.toLowerCase() === 'basic' && credential[0] === process.env.USERNAME && credential[1] === process.env.PASSWORD) {
+          // Basic auth suceeded, generate a token
+          let t = (new Date()).getTime() + crypto.randomBytes(243).toString('hex').trim()
+
+          app.tokens.push(t)
+
+          let out = {
+            token: t,
+            expires: (new Date()).getTime() + validTokenDuration
+          }
+
+          // Auto-remove tokens when they expire.
+          setTimeout(function () {
+            if (app.tokens.indexOf(t) >= 0) {
+              app.tokens = app.tokens.splice(app.tokens.indexOf(t), 1)
+            }
+          }, validTokenDuration)
+
+          return res.json(out)
+        }
+        return res.sendStatus(401)
+      } catch (e) {
+        res.sendStatus(401)
+      }
+    }
+  }
+}
